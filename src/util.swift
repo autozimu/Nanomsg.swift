135469d7b9ca9606b03adb52b051795c9ee1ae71
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-05-08 18:27:09 -0400

    Implement errno and strerror as extension.

--- src/util.swift
+++ src/util.swift
@@ -1,15 +1,5 @@
 import CNanomsg
 
-// Retrieve the current error.
-var errno: CInt {
-    return nn_errno()
-}
-
-// Convert an error number into human-readable string.
-func strerror(errno: CInt) -> String? {
-    return String(cString: nn_strerror(errno))
-}
-
 // Query the names and values of nanomsg symbols.
 func symbol(index: CInt) -> (CInt, String?) {
     var i: CInt = 0
