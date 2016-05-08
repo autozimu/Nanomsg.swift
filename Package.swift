5f0b235ed352bb932cb0ef9e66c9b6ced94564af
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-05-08 18:28:13 -0400

    Update CNanomsg url.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Nanomsg",
     dependencies: [
-        .Package(url: "../CNanomsg", majorVersion: 0)
+        .Package(url: "https://github.com/autozimu/CNanomsg.git", majorVersion: 0)
     ]
 )
