6bbe7c17aa91d10b5b341cf08d342ba29deaa817
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-05-09 11:38:04 -0400

    Add partial doc.

--- src/Constants.swift
+++ src/Constants.swift
@@ -1,8 +1,22 @@
 import CNanomsg
 
-public let VERSION_CURRENT = NN_VERSION_CURRENT
-public let VERSION_REVISION = NN_VERSION_REVISION
-public let VERSION_AGE = NN_VERSION_AGE
+/**
+ Current interface version.
+ */
+public let NANOMSG_VERSION_CURRENT = NN_VERSION_CURRENT
 
-// NN_MSG = ((size_t) - 1)
+/**
+ Revision of current interface.
+ */
+public let NANOMSG_VERSION_REVISION = NN_VERSION_REVISION
+
+/**
+ How many past interface versions are still supported.
+ */
+public let NANOMSG_VERSION_AGE = NN_VERSION_AGE
+
+/**
+ Zero-copy message size.
+ */
 let NN_MSG = 4294967295
+// NN_MSG = ((size_t) - 1)
