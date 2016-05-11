8081fc3b8ca4e9e565792e5bd6118b67dbef4735
Jeff Li <junfengl@ca.ibm.com>
2016-05-11 11:57:17 -0400

    Unwrap optional.

--- src/util.swift
+++ src/util.swift
@@ -3,6 +3,9 @@ import CNanomsg
 /** Query the names and values of nanomsg symbols. */
 public func symbol(index: CInt) -> (CInt, String?) {
     var i: CInt = 0
-    let cstr = nn_symbol(index, &i)
-    return (i, String(cString: cstr))
+    if let cstr = nn_symbol(index, &i) {
+        return (i, String(cString: cstr))
+    } else {
+        return (i, nil)
+    }
 }
