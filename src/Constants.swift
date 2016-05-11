5aba064f310acd9dd7d032b2278124e830b86920
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-05-10 21:01:34 -0400

    Keep constants namings consistent.

--- src/Constants.swift
+++ src/Constants.swift
@@ -1,13 +1,13 @@
 import CNanomsg
 
 /** Current interface version. */
-public let NANOMSG_VERSION_CURRENT = NN_VERSION_CURRENT
+public let NN_VERSION_CURRENT = CNanomsg.NN_VERSION_CURRENT
 
 /** Revision of current interface. */
-public let NANOMSG_VERSION_REVISION = NN_VERSION_REVISION
+public let NN_VERSION_REVISION = CNanomsg.NN_VERSION_REVISION
 
 /** How many past interface versions are still supported. */
-public let NANOMSG_VERSION_AGE = NN_VERSION_AGE
+public let NN_VERSION_AGE = CNanomsg.NN_VERSION_AGE
 
 /** Zero-copy message size. */
 let NN_MSG = 4294967295
